83ff555b309db29a9c7b31e990b29eac96398597
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-11 22:52:50 +0100

    refactor and style button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,11 @@
 import Card from '../Card/index.js';
 import './App.css';
 import React from 'react';
-import Button from '../Button/index.js';
 
 function App() {
 	return (
 		<div className='App'>
-			<Card title='Title 1' text='This is a card' />
-			<Button buttonText='see more' />
+			<Card title='Title 1' someText='This is a card' />
 		</div>
 	);
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,17 @@
 /*Inside index.js, create a functional component that returns a JSX element for your card.*/
+import Button from '../Button';
 
 export default function Card(props) {
 	return (
 		<div className='card'>
 			<h3>{props.title}</h3>
-			<p>{props.text}</p>
+			<p>{props.someText}</p>
 			<img
 				alt='soc'
 				width={'100%'}
 				src={'https://i.ytimg.com/vi/hHwblpw3grg/maxresdefault.jpg'}
 			/>
+			<Button buttonText='See more' />
 		</div>
 	);
 }
